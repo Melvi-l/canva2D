c305988116b993b0f3d420c932178777c72501b0
melvi-l <melvil.miglio@gmail.com>
2023-09-12 11:00:27 +0200

    can create polygon

--- 1convex_hull/geom.py
+++ 1convex_hull/geom.py
@@ -1,16 +1,26 @@
 from typing import List
 
 
-class Point:
+class Vertex:
     def __init__(self, x: float, y: float) -> None:
         self.x = x
         self.y = y
         
 class Segment:
-    def __init__(self, a: Point, b: Point) -> None:
+    def __init__(self, a: Vertex, b: Vertex) -> None:
         self.a = a
         self.b = b
 
 class Polygon: 
     def __init__(self, segmentList: List[Segment]) -> None:
         self.segmentList = segmentList
+        self.isClose = False
+
+    def addVertex(self, b: Vertex) -> None:
+        a = self.segmentList[-1].b
+        self.segmentList.append(Segment(a,b))
+
+    def close(self) -> None:
+        self.addVertex(self.segmentList[0].a)
+        self.isClose = True
+    
--- 1convex_hull/main.py
+++ 1convex_hull/main.py
@@ -1,40 +1,85 @@
 import pygame
 import sys
-from geom import Point
+from geom import Vertex, Segment, Polygon
 
 pygame.init()
 
-width = 800
-height = 600
+width = 1000
+height = 1000
 
 canvas = pygame.display.set_mode((width, height))
 
 background = (0, 0, 0)
 
-pointList = []
 
+polygon: Polygon = None
+lastVertex: Vertex = None
+
+def addVertexToPolygon(vertex: Vertex) -> None:
+    # prevent weird scope python shit
+    global lastVertex
+    global polygon
+
+    if lastVertex is None:
+        lastVertex = vertex 
+        return
+    if polygon is None:
+        segment = Segment(lastVertex, vertex)
+        polygon = Polygon([segment])
+        drawSegment(segment)
+        return
+    polygon.addVertex(vertex)
+    drawSegment(polygon.segmentList[-1])
+
+# Event handler
 def mouseEventHandler(event):
     if event.button == 1:
         x, y = pygame.mouse.get_pos()
-        pointList.append(Point(x, y))
+        newVertex = Vertex(x, y)
+        
+        if polygon is None or not(polygon.isClose):
+            drawVertex(newVertex)
+            addVertexToPolygon(newVertex)
+            return
+        
+        drawVertex(newVertex, (255,255,255))
+            
+    if event.button == 3:
+        polygon.close()
+        drawSegment(polygon.segmentList[-1])
+def keyboardEventHandler(event):
+    if event.key == pygame.K_SPACE:
+        print("space")
 
-# Game loop
-while True:
+# Draw function
+def drawVertex(vertex: Vertex, color = (255,0,0)):
+    pygame.draw.circle(canvas, color, (vertex.x, vertex.y), 2)
+def drawSegment(segment: Segment):
+    pygame.draw.line(canvas, (255,0,0), (segment.a.x, segment.a.y), (segment.b.x, segment.b.y), 1)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mouseEventHandler(event)
-    
+
+def init():
     canvas.fill(background)
 
-    for point in pointList: 
-        pygame.draw.circle(canvas, (255,0,0), (point.x, point.y), 2)
+def game():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouseEventHandler(event)
+            if event.type == pygame.KEYDOWN:
+                keyboardEventHandler(event)
+    
+        pygame.display.flip()
+
+init()
+game()
+     
         
 
+        
 
-    pygame.display.flip()
 
 
